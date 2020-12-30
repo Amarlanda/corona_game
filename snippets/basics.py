11debebef7ace88765b9ce032b8fb3c427fa9793
Rio <rajlanda5@gmail.com>
2020-12-30 16:14:58 +0000

    added some basic code for loops

--- snippets/basics.py
+++ snippets/basics.py
@@ -1,1 +1,23 @@
-a = int(input())
+a = 10
+b = 5
+x = c = a * (a - b) / b
+#print(x + 1111)
+
+a = ["5", "10", "15", "20"]
+
+total = 0 
+i = 0
+while i < 3:
+  print(a[i])
+  total = total + int(a[i])
+  i =  i + 1
+  print("inner loop: the total is :" , total)
+print("outer loop:the total is :" , total)
+
+a = 12
+b = 11
+if a > b:
+   print('a is greater than b')
+
+
+
